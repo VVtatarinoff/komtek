bf908a04f6b16fd827c77416d4bf2b28ec2640fb
vladimir <vvtatarinoff@gmail.com>
2022-03-30 21:41:10 +0300

    admin panel list of fields

--- references/admin.py
+++ references/admin.py
@@ -8,8 +8,16 @@ class RefTitlesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'short_name', 'description')
 
 
+class ElementsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ref_version', 'code', 'value')
+
+
+class RefVersionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reference', 'version', 'init_date')
+
+
 admin.site.register(RefTitles, RefTitlesAdmin)
-admin.site.register(RefVersions)
-admin.site.register(Elements)
+admin.site.register(RefVersions, RefVersionsAdmin)
+admin.site.register(Elements, ElementsAdmin)
 
 admin.site.site_title = 'Админ-панель справочников'
