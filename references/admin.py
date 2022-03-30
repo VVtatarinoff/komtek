b058e9867405133ec2b34405812d46631a490ee7
vladimir <vvtatarinoff@gmail.com>
2022-03-30 19:04:08 +0300

    models init

--- references/admin.py
+++ references/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from references.models import RefTitles, RefVersions, Elements
+
+
+class RefTitlesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'short_name', 'description')
+
+
+admin.site.register(RefTitles, RefTitlesAdmin)
+admin.site.register(RefVersions)
+admin.site.register(Elements)
+
+admin.site.site_title = 'Админ-панель справочников'
--- references/models.py
+++ references/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+
+class RefTitles(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name='Имя справочника')
+    short_name = models.CharField(max_length=50, unique=True, verbose_name='Короткое имя справочника')
+    description = models.TextField(verbose_name='Описание справочника')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Глобальные справочники'
+        verbose_name_plural = 'Глобальные справочники'
+        ordering = ['id']
+
+
+class RefVersions(models.Model):
+    reference = models.ForeignKey(RefTitles, on_delete=models.RESTRICT,
+                                  verbose_name='глобальный справочник')
+    version = models.CharField(max_length=50, verbose_name='Версия справочника')
+    init_date = models.DateTimeField(verbose_name='Дата начала действия')
+
+    def __str__(self):
+        return self.version
+
+    class Meta:
+        verbose_name = 'Версии справочников'
+        verbose_name_plural = 'Версии справочников'
+        ordering = ['id']
+
+
+class Elements(models.Model):
+    ref_version = models.ForeignKey(RefVersions, on_delete=models.RESTRICT,
+                                    verbose_name='версия справочника')
+    code = models.CharField(max_length=150, verbose_name='Код элемента')
+    value = models.CharField(max_length=150, verbose_name='Значение элемента')
+
+    class Meta:
+        verbose_name = 'Элементы справочника'
+        verbose_name_plural = 'Элементы справочника'
+        ordering = ['id']
