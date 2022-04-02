d47672050936bebecb0f51e0ba8b587bef5d4b86
vladimir <vvtatarinoff@gmail.com>
2022-04-02 14:45:47 +0300

    komtektest.herokuapp.com  allowed hosts

--- komtek_test/settings.py
+++ komtek_test/settings.py
@@ -33,8 +33,8 @@ DEBUG = (os.getenv('DEBUG') == 'True')
 # DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'ancient-gorge-78100.herokuapp.com',
-                 'www.ancient-gorge-78100.herokuapp.com',
+                 'komtektest.herokuapp.com',
+                 'www.komtektest.herokuapp.com',
                  '127.0.0.1',
                  '0.0.0.0'
                  ]
