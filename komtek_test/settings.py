cb71eb3c9b2771574f92f17160ebc4e1978bbdad
vladimir <vvtatarinoff@gmail.com>
2022-04-02 15:52:33 +0300

    whitenoise.storage.CompressedStaticFilesStorage

--- komtek_test/settings.py
+++ komtek_test/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'komtek_test')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
