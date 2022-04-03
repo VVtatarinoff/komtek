7544563a500eca52ceb01f6c2afdbde2d6b3fee8
vladimir <vvtatarinoff@gmail.com>
2022-04-03 16:10:39 +0300

    integrity error check

--- references/tests/test_db.py
+++ references/tests/test_db.py
@@ -1,6 +1,7 @@
 from copy import copy
 
 import pytest
+from django.db import IntegrityError
 
 from references.models import RefVersions
 from references.tests.fixtures.db_fixtures import REF_VERSIONS_TEST_CURRENT
@@ -10,11 +11,8 @@ from references.tests.fixtures.db_fixtures import REF_VERSIONS_TEST_CURRENT
 def test_create_dublicate_name_version(setup_ref_versions):
     new_ver = copy(REF_VERSIONS_TEST_CURRENT[0])
     new_ver.pop('id')
-    with pytest.raises(Exception) as e:
+    with pytest.raises(IntegrityError):
         RefVersions.objects.create(**new_ver)
-    assert (e.match(
-        'повторяющееся значение ключа') or e.match(
-        'duplicate key value violates unique constraint'))
 
 
 @pytest.mark.django_db
@@ -22,11 +20,8 @@ def test_create_dublicate_date_version(setup_ref_versions):
     new_ver = copy(REF_VERSIONS_TEST_CURRENT[0])
     new_ver.pop('id')
     new_ver['version'] += ' test date'
-    with pytest.raises(Exception) as e:
+    with pytest.raises(IntegrityError):
         RefVersions.objects.create(**new_ver)
-    assert (e.match(
-        'повторяющееся значение ключа') or e.match(
-        'duplicate key value violates unique constraint'))
 
 
 @pytest.mark.django_db
@@ -34,8 +29,5 @@ def test_create_null_version(setup_ref_versions):
     new_ver = copy(REF_VERSIONS_TEST_CURRENT[0])
     new_ver.pop('id')
     new_ver['version'] = ''
-    with pytest.raises(Exception) as e:
+    with pytest.raises(IntegrityError):
         RefVersions.objects.create(**new_ver)
-    assert (e.match(
-        'повторяющееся значение ключа') or e.match(
-        'duplicate key value violates unique constraint'))
